a700c951377c21e4f04973512526ced871b6dbe0
Bryan Frimin <bryan@getprobo.com>
2026-02-16 12:07:53 +0100

    Add Unref, RefOrNil and UnrefOrZero helpers
    Signed-off-by: Bryan Frimin <bryan@getprobo.com>

--- ref.go
+++ ref.go
@@ -3,3 +3,23 @@ package ref
 func Ref[T any](v T) *T {
 	return &v
 }
+
+func Unref[T any](v *T) T {
+	return *v
+}
+
+func RefOrNil[T comparable](v T) *T {
+	var zero T
+	if v == zero {
+		return nil
+	}
+	return &v
+}
+
+func UnrefOrZero[T any](v *T) T {
+	if v == nil {
+		var zero T
+		return zero
+	}
+	return *v
+}
